d306d29f1904734dad8b6ac2dc6715ccfa30d038
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-05-23 21:58:03 -0600

    feat: General project files

---
+++ my-app/src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calculator from './components/Calculator';
+import './globals.css';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Calculator />
+    </div>
+  );
+}
