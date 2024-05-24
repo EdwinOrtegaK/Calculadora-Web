962108e3458bd0ffe9fa2890761613d18c702332
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-05-24 00:04:25 -0600

    fix: Changes reverted to something functional

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,21 +1,8 @@
-import Critters from 'critters-webpack-plugin';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
   experimental: {
     optimizeCss: true,
-  },
-  webpack(config, { isServer }) {
-    if (isServer) {
-    }
-    config.plugins.push(
-      new Critters({
-        preload: 'js-lazy',
-        noscriptFallback: true,
-      })
-    );
-    return config;
   },
 };
 
