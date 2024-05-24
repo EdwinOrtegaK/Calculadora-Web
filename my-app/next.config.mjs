0ed9782f5addd557a7c6d0f0cf727df3e680b7a7
Edwin Ortega <andyortegakou@yahoo.com.mx>
2024-05-23 22:14:03 -0600

    feat: Files used for critters

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,10 +1,23 @@
+import Critters from 'critters';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    swcMinify: true,
-    experimental: {
-      optimizeCss: true,
-    },
-  };
-  
-  export default nextConfig;
-  
+  swcMinify: true,
+  experimental: {
+    optimizeCss: true,
+  },
+  webpack(config, { isServer }) {
+    if (isServer) {
+      require('./scripts/generate-sitemap');
+    }
+    config.plugins.push(
+      new Critters({
+        preload: 'js-lazy',
+        noscriptFallback: true,
+      })
+    );
+    return config;
+  },
+};
+
+export default nextConfig;
